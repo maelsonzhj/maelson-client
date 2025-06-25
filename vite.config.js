21ff274477f730f0bbf7c339dc39b0b0adce1a1d
Makson <muhammedzanzak@gmail.com>
2025-06-25 13:19:55 +0500

    Fix GitHub Pages paths and deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	base: '/maelson-client/',
 	plugins: [react()],
+	base: '/maelson-client/',
 })
